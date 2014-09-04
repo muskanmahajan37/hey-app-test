672a8de60ea7ee979b2dde35388eb6522436daa3
Eric <erjohnson.pdx@gmail.com>
2014-09-04 05:05:37 +0000

    Create user sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+
+  def create
+    user = User.find_by_name(params[:name])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: "Welcome, #{user.name}!"
+    else
+      flash.now.alert = "Name or password is invalid"
+      render "new"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: "Logged out!"
+  end
+end
