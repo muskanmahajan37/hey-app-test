f2b70c9d28df51e84141917ce0bfda8ed0b9bfc0
Eric <erjohnson.pdx@gmail.com>
2014-09-04 05:21:50 +0000

    Create Locations controller

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,28 @@
+class LocationsController < ApplicationController
+  def index
+    @locations = Location.all
+  end
+
+  def new
+    @location = Location.new
+  end
+
+  def create
+    @location = Location.new(location_params)
+    if @location.save
+      session[:location_id] = @location.id
+      redirect_to locations_path, notice: "Thank you for signing in."
+    else
+      render "new"
+    end
+  end
+
+  def show
+    @location = Location.find(params[:id])
+  end
+
+  private
+    def location_params
+      params.require(:location).permit(:name)
+    end
+end
