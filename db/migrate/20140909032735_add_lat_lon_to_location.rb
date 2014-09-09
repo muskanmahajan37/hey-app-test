6f370049a3e317ef7fd0b4b38644e9479e0fa3bd
Eric <erjohnson.pdx@gmail.com>
2014-09-08 20:32:49 -0700

    add latitude & longitude to location model

---
+++ db/migrate/20140909032735_add_lat_lon_to_location.rb
@@ -0,0 +1,6 @@
+class AddLatLonToLocation < ActiveRecord::Migration
+  def change
+    add_column :locations, :latitude, :string
+    add_column :locations, :longitude, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140904051428) do
+ActiveRecord::Schema.define(version: 20140909032735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20140904051428) do
     t.string   "city"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "latitude"
+    t.string   "longitude"
   end
 
   create_table "users", force: true do |t|
